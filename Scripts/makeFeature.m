9cac30d9b2964dbc1c54a3aba72ff130c565d89a
VMinhoto <33106297+VMinhoto@users.noreply.github.com>
2017-11-25 18:16:48 +0000

    Add files via upload

---
+++ Scripts/makeFeature.m
@@ -0,0 +1,13 @@
+function [feature,N]=makeFeature(lacunas,N);
+    N=N*24;
+    CounterBehind=N;
+    feature=zeros(length(lacunas),1);
+    for i=1:length(lacunas)
+        if (CounterBehind>0)
+            feature(i)=nan;
+            CounterBehind=CounterBehind-1;
+        else 
+            feature(i)=lacunas(i-N);
+        end
+    end
+end
---
+++ Scripts/makeMatrix.m
@@ -0,0 +1,17 @@
+function [matrix]=makeMAtrix(varargin)
+
+a=nargin;
+if nargin == 0
+    error('No elements inserted')
+end
+
+lengthElement=length(varargin{1});
+matrix=zeros(lengthElement,nargin);
+for i=1:a
+    if (length(varargin{i}) ~= lengthElement)
+        error('Elements have wrong size');
+    end
+    matrix(:,i)=varargin{i};
+end
+
+end
