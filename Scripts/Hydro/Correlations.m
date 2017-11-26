842fe449a7a745d9216c9e1adab17263aa6b156b
iitriigo <33739212+iitriigo@users.noreply.github.com>
2017-11-26 16:38:34 +0000

    Add files via upload

---
+++ Scripts/Hydro/Correlations.m
@@ -0,0 +1,37 @@
+
+figure;
+scatter(Month, Hydro);
+title('Hydro vs Month');
+
+figure;
+scatter(Week, Hydro);
+title('Hydro vs Week');
+
+figure;
+scatter(Hour, Hydro);
+title('Hydro vs Hour');
+
+figure;
+scatter(Temp, Hydro);
+title('Hydro vs Temperature');
+
+figure;
+scatter(Wind, Hydro);
+title('Hydro vs Wind');
+
+figure;
+scatter(WindDirc, Hydro);
+title('Hydro vs WindDirection');
+
+figure;
+scatter(Irradiance, Hydro);
+title('Hydro vs Irradiance');
+
+figure;
+scatter(Rainfall, Hydro);
+title('Hydro vs Rain');
+
+
+
+
+
