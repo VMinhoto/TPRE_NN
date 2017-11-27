bd60b00252e4d3e18709be897b815ba4c58f42c9
iitriigo <33739212+iitriigo@users.noreply.github.com>
2017-11-27 15:33:59 +0000

    Add files via upload

---
+++ Scripts/Nuclear/Correlations.m
@@ -0,0 +1,37 @@
+
+figure;
+scatter(Month, Nuc);
+title('Nuclear vs Month');
+
+figure;
+scatter(Week, Nuc);
+title('Nuclear vs Week');
+
+figure;
+scatter(Hour, Nuc);
+title('Nuclear vs Hour');
+
+figure;
+scatter(Temp, Nuc);
+title('Nuclear vs Temperature');
+
+figure;
+scatter(Wind, Nuc);
+title('Nuclear vs Wind');
+
+figure;
+scatter(WindDirc, Nuc);
+title('Nuclear vs WindDirection');
+
+figure;
+scatter(Irradiance, Nuc);
+title('Nuclear vs Irradiance');
+
+figure;
+scatter(Rainfall, Nuc);
+title('Nuclear vs Rain');
+
+
+
+
+
