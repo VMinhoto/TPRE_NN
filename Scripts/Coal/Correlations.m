743151c1c89c4c3040f11e696a26593714f7d771
iitriigo <33739212+iitriigo@users.noreply.github.com>
2017-11-27 22:46:00 +0000

    Add files via upload

---
+++ Scripts/Coal/Correlations.m
@@ -0,0 +1,37 @@
+
+figure;
+scatter(Month, Coal);
+title('Coal vs Month');
+
+figure;
+scatter(Week, Coal);
+title('Coal vs Week');
+
+figure;
+scatter(Hour, Coal);
+title('Coal vs Hour');
+
+figure;
+scatter(Temp, Coal);
+title('Coal vs Temperature');
+
+figure;
+scatter(Wind, Coal);
+title('Coal vs Wind');
+
+figure;
+scatter(WindDirc, Coal);
+title('Coal vs WindDirection');
+
+figure;
+scatter(Irradiance, Coal);
+title('Coal vs Irradiance');
+
+figure;
+scatter(Rainfall, Coal);
+title('Coal vs Rain');
+
+
+
+
+
