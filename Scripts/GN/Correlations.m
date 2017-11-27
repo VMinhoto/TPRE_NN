7b70663ab7a8576117875be07d80f232f63234d1
iitriigo <33739212+iitriigo@users.noreply.github.com>
2017-11-27 23:05:00 +0000

    Add files via upload

---
+++ Scripts/GN/Correlations.m
@@ -0,0 +1,37 @@
+
+figure;
+scatter(Month, GN);
+title('Natural Gas vs Month');
+
+figure;
+scatter(Week, GN);
+title('Natural Gas vs Week');
+
+figure;
+scatter(Hour, GN);
+title('Natural Gas vs Hour');
+
+figure;
+scatter(Temp, GN);
+title('Natural Gas vs Temperature');
+
+figure;
+scatter(Wind, GN);
+title('Natural Gas vs Wind');
+
+figure;
+scatter(WindDirc, GN);
+title('Natural Gas vs WindDirection');
+
+figure;
+scatter(Irradiance, GN);
+title('Natural Gas vs Irradiance');
+
+figure;
+scatter(Rainfall, GN);
+title('Natural Gas vs Rain');
+
+
+
+
+
