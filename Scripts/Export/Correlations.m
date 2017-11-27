09d4a56b1d0172adf50e4215e67f782dc220a22b
iitriigo <33739212+iitriigo@users.noreply.github.com>
2017-11-27 23:21:35 +0000

    Add files via upload

---
+++ Scripts/Export/Correlations.m
@@ -0,0 +1,37 @@
+
+figure;
+scatter(Month, Export);
+title('Exportation vs Month');
+
+figure;
+scatter(Week, Export);
+title('Exportation vs Week');
+
+figure;
+scatter(Hour, Export);
+title('Exportation vs Hour');
+
+figure;
+scatter(Temp, Export);
+title('Exportation vs Temperature');
+
+figure;
+scatter(Wind, Export);
+title('Exportation vs Wind');
+
+figure;
+scatter(WindDirc, Export);
+title('Exportation vs WindDirection');
+
+figure;
+scatter(Irradiance, Export);
+title('Exportation vs Irradiance');
+
+figure;
+scatter(Rainfall, Export);
+title('Exportation vs Rain');
+
+
+
+
+
