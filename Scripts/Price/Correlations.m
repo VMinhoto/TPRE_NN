a35ab56a621776f4b624097c0de758de138b85bb
iitriigo <33739212+iitriigo@users.noreply.github.com>
2017-11-27 23:28:31 +0000

    Add files via upload

---
+++ Scripts/Price/Correlations.m
@@ -0,0 +1,37 @@
+
+figure;
+scatter(Month, Price);
+title('Price vs Month');
+
+figure;
+scatter(Week, Price);
+title('Price vs Week');
+
+figure;
+scatter(Hour, Price);
+title('Price vs Hour');
+
+figure;
+scatter(Temp, Price);
+title('Price vs Temperature');
+
+figure;
+scatter(Wind, Price);
+title('Price vs Wind');
+
+figure;
+scatter(WindDirc, Price);
+title('Price vs WindDirection');
+
+figure;
+scatter(Irradiance, Price);
+title('Price vs Irradiance');
+
+figure;
+scatter(Rainfall, Price);
+title('Price vs Rain');
+
+
+
+
+
