fa962e99a56157689d3459049a80dc4b30b2c7e0
iitriigo <33739212+iitriigo@users.noreply.github.com>
2017-11-29 16:22:43 +0000

    Update Correlations.m

--- Scripts/CG-Solar/Correlations.m
+++ Scripts/CG-Solar/Correlations.m
@@ -1,33 +1,24 @@
-
-figure;
 drawcorrelation(Month, CG_Solar,1);
 title('CG_Solar vs Month');
 
-figure;
 drawcorrelation(Week, CG_Solar,1);
 title('CG_Solar vs Week');
 
-figure;
 drawcorrelation(Hour, CG_Solar,1);
 title('CG_Solar vs Hour');
 
-figure;
 drawcorrelation(Temp, CG_Solar,1);
 title('CG_Solar vs Temperature');
 
-figure;
 drawcorrelation(Wind, CG_Solar,1);
 title('CG_Solar vs Wind');
 
-figure;
 drawcorrelation(WindDirc, CG_Solar,1);
 title('CG_Solar vs WindDirection');
 
-figure;
 drawcorrelation(Irradiance, CG_Solar,1);
 title('CG_Solar vs Irradiance');
 
-figure;
 drawcorrelation(Rainfall, CG_Solar,1);
 title('CG_Solar vs Rain');
 
